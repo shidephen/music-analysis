9f9a4f9e45f94a3ff88137004b080c999b6a67a2
shidephen <shidephen@outlook.com>
2016-10-12 16:42:00 +0800

    增加获取高潮节拍信息的方法

--- tempo.py
+++ tempo.py
@@ -14,11 +14,10 @@ SAMPLING_RATE = 44100.0
 QUARTER_SIZE = 0.25
 
 
-def detect_tempo(beats_file):
-    beats = np.loadtxt(beats_file)
+def detect_tempo(beatline):
 
-    p1_beats = beats[1:]
-    n1_beats = beats[0: -1]
+    p1_beats = beatline[1:]
+    n1_beats = beatline[0: -1]
     offset = p1_beats - n1_beats
 
     return np.round(1 / np.mean(offset) * 60)
@@ -35,12 +34,16 @@ def pad_beats(notes, bpm, beat_size=QUARTER_SIZE, fs=SAMPLING_RATE, block_size=B
     :return:
     """
 
-    interval = float(block_size) / fs
+    interval = float(block_size) / fs # sampling intervals
     notes_chroma = []
     notes_count = len(notes)
     chroma_sum = 0
 
     for n in range(notes_count):
+        """
+        From statistics, the max chroma intensity for a note is about 3, the min intensity is 0.
+        For a  note from midi, all chroma in a duration is 3 and for a rest is 0.
+        """
         if notes[n].isClassOrSubclass((note.Note,)):
             chroma = np.zeros(12)
             chroma[np.where(music21_tone_map == notes[n].name)[0][0]] = 3
@@ -62,3 +65,25 @@ def pad_beats(notes, bpm, beat_size=QUARTER_SIZE, fs=SAMPLING_RATE, block_size=B
 
     # print('Total insert %d chromas' % chroma_sum)
     return np.array(notes_chroma)
+
+
+def get_climax(music_info):
+    """
+    获取高潮部分的拍号
+    :param music_info: music_info文件路径
+    :return:
+    """
+    s_climax = None
+    with open(music_info) as f:
+        lines = f.readlines()
+        for l in lines:
+            if 'Climax:' in l:
+                s_climax = l
+
+    if s_climax is None:
+        return None
+
+    s_climax = s_climax.strip()
+    s_climax = s_climax[s_climax.find(':') + 1:]
+
+    return map(lambda x: int(x), s_climax.split(','))
