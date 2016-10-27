0a8ece2616232a24102cc25a921e7f426821fee1
shidephen <shidephen@outlook.com>
2016-10-27 19:45:40 +0800

    增加chroma生成程序对返回值的判断

--- extract_chroma/__init__.py
+++ extract_chroma/__init__.py
@@ -12,6 +12,12 @@ exe = os.path.join(os.path.dirname(__file__), 'extractchroma.exe')
 
 
 def gen_chroma(filepath):
+    """
+    生成音频chroma信息
+    :param filepath: 音频文件路径(必须是wav)
+    :return: chroma文件路径(chroma, basschroma)
+    """
+    assert(filepath.endswith('.wav'))
     command = '%s %s' % (exe, filepath)
     chroma = str(filepath).replace('.wav', '_chroma.csv')
     basschroma = str(filepath).replace('.wav', '_basschroma.csv')
@@ -19,5 +25,8 @@ def gen_chroma(filepath):
     if os.path.exists(chroma) and os.path.exists(basschroma):
         return chroma, basschroma
 
-    subprocess.call(command)
+    ret = subprocess.call(command)
+    if ret != 0:
+        return None, None
+
     return chroma, basschroma
