fb182445478bbe041b245e4a5db6b77b5cf13657
shidephen <shidephen@outlook.com>
2016-10-28 18:17:37 +0800

    增加几个工作目录配置项

--- config.py
+++ config.py
@@ -6,10 +6,22 @@ Created on Sun Sep 18 17:01:37 2016
 """
 
 # Output directories
+MUSIC_PATH = r'D:\Last\Songs'
 CLIP_PATH = r'D:\Last\Clips'
 CHORD_PATH = r'D:\Last\Songs'
 BEAT_PATH = r'D:\Last\Songs'
 MATRIX_PATH = r'D:\Last\Data'
+MUSIC_INFO_PATH = r'D:\Last\Songs'
+
+# Naming suffices
+CHROMA_SUFFIX = '_chroma.csv'
+BASSCHROMA_SUFFIX = '_basschroma.csv'
+BEAT_SUFFIX = '_beat.txt'
+INFO_SUFFIX = '_info.txt'
+CHORD_SUFFIX = '_chord.txt'
+# clip id and its score matrix
+ID_MATRIX_SUFFIX = '_clips.txt'
+SCORES_MATRIX_SUFFIX = '_scores.txt'
 
 # Temparay data directories
 CHROMA_PATH = r'D:\Last\Temp'
