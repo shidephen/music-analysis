0e8eacdc20b5993334f67ba6849fee0ea911752f
shidephen <shidephen@outlook.com>
2016-11-01 16:36:45 +0800

    1. 增加转换成WAV的方法 2. 增加组装MIDI文件的方法

--- format.py
+++ format.py
@@ -6,18 +6,49 @@ Created on Wed Sep 21 14:28:03 2016
 """
 
 import os, os.path
-import pydub
+from pydub import AudioSegment
+from music21 import *
 
 
-def convert2wav(dir):
-    for root, sub, files in os.walk(dir):
-        for file in files:
-            filepath = os.path.abspath(os.path.join(root, file))
-            if filepath.endswith('mp3'):
-                print('converting %s\n' % filepath)
-                pos = filepath.rfind('.')
-                outputfile = filepath[0: pos] + '.wav'
-                clip = pydub.AudioSegment.from_file(filepath)
-                clip.export(outputfile, format='wav')
-                os.remove(filepath)
+def convert2wav(filepath):
+    """
+    将音频文件转换成wav
+    :param filepath: 文件路径
+    :return: 输出文件路径， 文件时长
+    """
+    if not os.path.exists(filepath):
+        return None, 0
+
+    if filepath.endswith('.wav'):
+        clip = AudioSegment.from_file(filepath)
+        return filepath, clip.duration_seconds
+
+    pos = filepath.rfind('.')
+    outputfile = filepath[0: pos] + '.wav'
+    clip = AudioSegment.from_file(filepath)
+    clip.export(outputfile, format='wav')
+    # os.remove(filepath)
+    return outputfile, clip.duration_seconds
+
+
+def construct_midi(notes, tempo_rate):
+    """
+    将音符流转化成MIDI文件
+    :param notes: 音符
+    :param tempo_rate: BPM
+    :return:
+    """
+    m = tempo.MetronomeMark(number=tempo_rate)
+    p = instrument.Piano()
+    t = meter.TimeSignature('4/4')
+
+    s = stream.Part()
+    s.append(p)
+    s.append(t)
+    s.append(m)
+
+    for n in s.notesAndRests:
+        s.append(n)
+
+    return s
 
